adac853b40cc525c6fd1ad64fdea6abb5a73a281
sbcchang <iamsbc@gmail.com>
2022-06-13 22:26:05 +0800

    加入專案檔案。

---
+++ WpfBMI/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfBMI
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, RoutedEventArgs e)
+        {
+            double height, weight;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(inHeight.Text) &&
+                    !string.IsNullOrEmpty(inWeight.Text) &&
+                    double.TryParse(inHeight.Text, out height) &&
+                    double.TryParse(inWeight.Text, out weight))
+                {
+                    calResult.Text = (weight / Math.Pow(height / 100, 2)).ToString();
+                }
+                else
+                {
+                    calResult.Text = "輸入有誤";
+                }
+            }
+            catch (Exception ex)
+            {
+                calResult.Text = $"錯誤:{ex}";
+            }
+        }
+    }
+}
